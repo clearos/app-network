2b2a399a146ec98117b0028af91d4f81d9d6ddf2
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-04-08 20:04:30 +0000

    - Integrated graphic assets [tracker #542]

--- views/wizard/mode.php
+++ views/wizard/mode.php
@@ -36,15 +36,44 @@
 $this->lang->load('base');
 $this->lang->load('network');
 
+$read_only = FALSE;
+
+///////////////////////////////////////////////////////////////////////////////
+// Content
+///////////////////////////////////////////////////////////////////////////////
+
+// TODO: translate
+// TODO: move HTML/CSS elements to theme
+
+$gateway_label = "<span style='font-size: 13px; font-weight: bold;'>Gateway Mode</span>";
+$gateway_options['image'] = clearos_app_htdocs('network') . '/gateway.png';
+$gateway_options['label_help'] = "<p style='font-size: 13px;  padding-left: 25px'>Gateway mode is used to connect a network of systems to the Internet or internal network.  You need two network cards for this mode.</p>";
+
+$private_label = "<span style='font-size: 13px; font-weight: bold;'>Private Server Mode (No Firewall)</span>";
+$private_options['image'] = clearos_app_htdocs('network') . '/private_server.png';
+$private_options['label_help'] = "<p style='font-size: 13px;  padding-left: 25px'>This mode is appropriate for standalone servers installed on a protected network, for example, an office network.  The firewall on this system is disabled in this mode!</p>";
+
+$public_label = "<span style='font-size: 13px; font-weight: bold;'>Public Server Mode</span>";
+$public_options['image'] = clearos_app_htdocs('network') . '/public_server.png';
+$public_options['label_help'] = "<p style='font-size: 13px; padding-left: 25px;'>This mode is appropriate for standalone servers installed in hostile environment, for example a data center or public hotspot.</p>";
+
+$checked[$network_mode] = TRUE;
+
 ///////////////////////////////////////////////////////////////////////////////
 // Form
 ///////////////////////////////////////////////////////////////////////////////
-// Note: the wizard previous/next buttons are used for form submission 
 
-echo form_open('network/mode', array('id' => 'mode_form')); 
+echo form_open('network/mode', array('id' => 'mode_form'));
 echo form_header(lang('base_settings'));
 
-echo field_dropdown('network_mode', $network_modes, $network_mode, lang('network_network_mode'));
+echo field_radio_set(
+    '',
+    array(
+        field_radio_set_item('gateway', 'network_mode', $gateway_label, $checked['gateway'], $read_only, $gateway_options),
+        field_radio_set_item('trustedstandalone', 'network_mode', $private_label, $checked['trustedstandalone'], $read_only, $private_options),
+        field_radio_set_item('standalone', 'network_mode', $public_label, $checked['standalone'], $read_only, $public_options),
+    )
+);
 
 echo form_footer();
 echo form_close();
