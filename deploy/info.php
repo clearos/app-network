ef634bb3c9c8c808a826cd0140f239e64ef7d7c1
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-10-07 01:25:55 +0000

    - Added translation

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'network';
-$app['version'] = '1.2.8';
+$app['version'] = '1.2.9';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
