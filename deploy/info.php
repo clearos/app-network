9a4c0584001e220391c51af4ab5b4734c2d386bb
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-10-16 17:42:27 +0000

    - Removed IPv6 reference in /etc/hosts

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'network';
-$app['version'] = '1.2.9';
+$app['version'] = '1.2.10';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
