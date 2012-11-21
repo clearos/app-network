e78f3186e8ef6a5a7d302e734e147587afea8fd1
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-11-21 22:29:24 +0000

    - Added network and firewall check widget [tracker #829]

---
+++ controllers/network_check.php
@@ -0,0 +1,175 @@
+<?php
+
+/**
+ * Network check controller.
+ *
+ * @category   Apps
+ * @package    Network
+ * @subpackage Controllers
+ * @author     ClearFoundation <developer@clearfoundation.com>
+ * @copyright  2012 ClearFoundation
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License version 3 or later
+ * @link       http://www.clearfoundation.com/docs/developer/apps/network/
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// D E P E N D E N C I E S
+///////////////////////////////////////////////////////////////////////////////
+
+use \clearos\apps\firewall\Firewall as Firewall;
+
+///////////////////////////////////////////////////////////////////////////////
+// C L A S S
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Network check controller.
+ *
+ * @category   Apps
+ * @package    Network
+ * @subpackage Controllers
+ * @author     ClearFoundation <developer@clearfoundation.com>
+ * @copyright  2012 ClearFoundation
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License version 3 or later
+ * @link       http://www.clearfoundation.com/docs/developer/apps/network/
+ */
+
+class Network_Check extends ClearOS_Controller
+{
+    protected $app_name = NULL;
+    protected $protocol = NULL;
+    protected $port_number = NULL;
+
+    /**
+     * Network check constructor.
+     *
+     * @param string $app_name app basename
+     *
+     * @return view
+     */
+
+    function __construct($app_name, $protocol = NULL, $port = NULL)
+    {
+        $this->app_name = $app_name;
+        $this->protocol = $protocol;
+        $this->port_number = $port;
+    }
+
+    /**
+     * Network check view.
+     *
+     * @return view
+     */
+
+    function index($protocol = NULL, $port = NULL)
+    {
+
+        // SSH and maybe some others have floating protocol an port numbers
+        // Added a way to override these on the fly instead of using the
+        // constructor.
+
+        // Load dependencies
+        //------------------
+
+        if (! empty($port))
+            $this->port_number = $port;
+
+        if (! empty($protocol))
+            $this->protocol = $protocol;
+
+        $this->lang->load('network');
+
+        if (clearos_app_installed('incoming_firewall'))
+            $this->load->library('incoming_firewall/Port');
+    
+        // Load view data
+        //---------------
+
+        try {
+            if (clearos_app_installed('incoming_firewall'))
+                $is_firewalled = $this->port->is_firewalled($this->protocol, $this->port_number);
+            else
+                $is_firewalled = TRUE;
+        } catch (Exception $e) {
+            $this->page->view_exception($e);
+            return;
+        }
+
+        $data['app_name'] = $this->app_name;
+        $data['protocol'] = $this->protocol;
+        $data['port'] = $this->port_number;
+
+        // Load views
+        //-----------
+
+        if (! $is_firewalled)
+            return;
+
+        $this->page->view_form('network/check', $data, lang('network_network_check'));
+    }
+
+    /**
+     * Network add to incoming firewall.
+     *
+     * @param string $protocol protocol
+     * @param string $port     port
+     * @return view
+     */
+
+    function add($protocol, $port = NULL)
+    {
+        // Load libraries
+        //---------------
+
+        if (!(clearos_app_installed('incoming_firewall')))
+            return;
+
+        $this->load->library('incoming_firewall/Port');
+        $this->load->library('incoming_firewall/Incoming');
+        $this->lang->load('incoming_firewall');
+
+        // Handle form submit
+        //-------------------
+
+        // TODO: remove PPTP/IPsec hacks
+        if ($protocol == 'PPTP') {
+            $this->incoming->add_allow_standard_service('PPTP');
+        } else if ($protocol == 'IPsec') {
+            $this->incoming->add_allow_standard_service('IPsec');
+        } else {
+            if ($this->incoming->check_port($protocol, $port) == Firewall::CONSTANT_DISABLED)
+                $this->incoming->set_allow_port_state(TRUE, $protocol, $port);
+            else
+                $this->incoming->add_allow_port($this->app_name, $protocol, $port);
+        }
+
+        // Load view data
+        //---------------
+
+        $this->page->set_status_updated();
+
+        $this->load->library('user_agent');
+
+        if ($this->agent->is_referral())
+            redirect($this->agent->referrer());
+        else
+            redirect('/incoming_firewall/allow');
+    }
+}
---
+++ libraries/Network_Check.php
@@ -0,0 +1,113 @@
+<?php
+
+/**
+ * Network check class.
+ *
+ * @category   Apps
+ * @package    Network
+ * @subpackage Libraries
+ * @author     ClearFoundation <developer@clearfoundation.com>
+ * @copyright  2012 ClearFoundation
+ * @license    http://www.gnu.org/copyleft/lgpl.html GNU Lesser General Public License version 3 or later
+ * @link       http://www.clearfoundation.com/docs/developer/apps/network/
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// N A M E S P A C E
+///////////////////////////////////////////////////////////////////////////////
+
+namespace clearos\apps\network;
+
+///////////////////////////////////////////////////////////////////////////////
+// B O O T S T R A P
+///////////////////////////////////////////////////////////////////////////////
+
+$bootstrap = getenv('CLEAROS_BOOTSTRAP') ? getenv('CLEAROS_BOOTSTRAP') : '/usr/clearos/framework/shared';
+require_once $bootstrap . '/bootstrap.php';
+
+///////////////////////////////////////////////////////////////////////////////
+// T R A N S L A T I O N S
+///////////////////////////////////////////////////////////////////////////////
+
+clearos_load_language('base');
+clearos_load_language('network');
+
+///////////////////////////////////////////////////////////////////////////////
+// D E P E N D E N C I E S
+///////////////////////////////////////////////////////////////////////////////
+
+use \clearos\apps\base\Engine as Engine;
+use \clearos\apps\base\File as File;
+
+clearos_load_library('base/Engine');
+clearos_load_library('base/File');
+
+///////////////////////////////////////////////////////////////////////////////
+// C L A S S
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Network check class.
+ *
+ * @category   Apps
+ * @package    Network
+ * @subpackage Libraries
+ * @author     ClearFoundation <developer@clearfoundation.com>
+ * @copyright  2012 ClearFoundation
+ * @license    http://www.gnu.org/copyleft/lgpl.html GNU Lesser General Public License version 3 or later
+ * @link       http://www.clearfoundation.com/docs/developer/apps/network/
+ */
+
+class Network_Check extends Engine
+{
+    ///////////////////////////////////////////////////////////////////////////////
+    // C O N S T A N T S
+    ///////////////////////////////////////////////////////////////////////////////
+
+    const FILE_STATS = '/proc/net/dev';
+    const FILE_STATE = '/var/clearos/network/state';
+
+    ///////////////////////////////////////////////////////////////////////////////
+    // M E T H O D S
+    ///////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Network check constructor.
+     */
+
+    public function __construct()
+    {
+        clearos_profile(__METHOD__, __LINE__);
+    }
+
+    /**
+     * Returns interface stats and rates.
+     *
+     * @return array interface stats and rates
+     * @throws Engine_Exception
+     */
+
+    public function incoming_check($app, $protocol, $check);
+    {
+        clearos_profile(__METHOD__, __LINE__);
+
+        return FALSE;
+    }
+}
