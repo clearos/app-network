674f33bc94ece323833fd8c185c4441a96ffc670
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-06-10 16:12:32 +0000

    - Fixed broken translation tag

--- libraries/Iface.php
+++ libraries/Iface.php
@@ -1899,7 +1899,7 @@ class Iface extends Engine
         clearos_profile(__METHOD__, __LINE__);
 
         if (! preg_match('/^[a-zA-Z0-9:\._]+$/', $interface))
-            return lang('network_network_interface_invalid' . $interface);
+            return lang('network_network_interface_invalid');
     }
 
     /**
