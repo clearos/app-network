739002e965893c09af4a6900f0fd827124f26cbb
Ben Chambers <bchambers@clearfoundation.com>
2013-09-09 16:42:39 +0000

    - Added language tags for firewall mode warning - Added view to display firewall disabled

---
+++ views/firewall_verify.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Network firewall sanity check view.
+ *
+ * @category   apps
+ * @package    network
+ * @subpackage views
+ * @author     ClearFoundation <developer@clearfoundation.com>
+ * @copyright  2011 ClearFoundation
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License version 3 or later
+ * @link       http://www.clearfoundation.com/docs/developer/apps/network/
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.  
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// Load dependencies
+///////////////////////////////////////////////////////////////////////////////
+
+$this->lang->load('network');
+$this->lang->load('base');
+
+echo infobox_warning(
+    lang('base_warning'), lang('network_firewall_disabled') .
+    "<div style='text-align: center; padding-top: 5px;'>" . anchor_custom('/app/network', lang('network_edit_mode')) . "</div>"
+);
